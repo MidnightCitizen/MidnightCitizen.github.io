ded517616e83af7e91748ae55865f64ac2cce54f
MidnightCitizen <themidnightcitizen@hotmail.fr>
2017-12-22 21:12:40 +0100

    JQuery, Typed.js, new text in the first page, trying to recreate a terminal

---
+++ js/style.js
@@ -0,0 +1,11 @@
+// Can also be included with a regular script tag
+
+
+var options = {
+  strings: ["",  "git push --force ^1000 <br> `pushed to origin with option force ^1000 `", "clear"],
+  typeSpeed: 50,
+  loop : true,
+  showCursor: true
+}
+
+var typed = new Typed("#presentation", options);
