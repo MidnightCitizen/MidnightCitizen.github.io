bbaa4ed0724c10a808cfa21f25f8c29b8ccb2589
MidnightCitizen <themidnightcitizen@hotmail.fr>
2018-02-21 20:11:51 +0100

    Add Loader

--- js/app.js
+++ js/app.js
@@ -2,3 +2,15 @@
 particlesJS.load('particles-js', 'assets/particles.json', function() {
   console.log('callback - particles.js config loaded');
 });
+
+
+var myVar;
+
+function myFunction() {
+    myVar = setTimeout(showPage, 2000);
+}
+
+function showPage() {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("myDiv").style.display = "block";
+}
